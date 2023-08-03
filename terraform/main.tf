9408740d1df23466818dbafbcc51bbaea38fffbf
Pepe Fagoaga <pepe@verica.io>
2023-08-03 17:56:52 +0200

    chore(terraform): Update terraform, providers and modules (#14)

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,11 +11,11 @@ variable "external_id" {
 # Terraform Provider Configuration
 ###################################
 terraform {
-  required_version = ">= 1.3.0"
+  required_version = ">= 1.5.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.10"
     }
   }
 }
